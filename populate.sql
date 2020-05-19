5441a9ed2f53d90758b90b5292342dc2a74d1e4b
marinalis1710 <62431328+marinalis1710@users.noreply.github.com>
2020-05-19 17:34:25 +0300

    Create populate.sql

---
+++ populate.sql
@@ -0,0 +1,76 @@
+insert into City(city_name)
+values ("Kyiv")
+insert into City(city_name)
+values ("Lviv")
+insert into City(city_name)
+values ("Odessa")
+
+insert into Barman(barman_name)
+values ("Barman Oleg")
+insert into Barman(barman_name)
+values ("Barman Vasya")
+insert into Barman(barman_name)
+values ("Barman Kolya")
+
+insert into Price(price)
+values (5)
+insert into Price(price)
+values (10)
+insert into Price(price)
+values (40)
+
+insert into Cocktail(cocktail_name)
+values ("Margarita")
+insert into Cocktail(cocktail_name)
+values ("Ocean beach")
+insert into Cocktail(cocktail_name)
+values ("Snow in winter")
+
+insert into Client(client_name)
+values ("Bob")
+insert into Client(client_name)
+values (")
+insert into Client(client_name)
+values
+
+insert into 
+values
+insert into 
+values
+insert into 
+values
+
+insert into 
+values
+insert into 
+values
+insert into 
+values
+
+insert into 
+values
+insert into 
+values
+insert into 
+values
+
+insert into 
+values
+insert into 
+values
+insert into 
+values
+
+insert into 
+values
+insert into 
+values
+insert into 
+values
+
+insert into 
+values
+insert into 
+values
+insert into 
+values
