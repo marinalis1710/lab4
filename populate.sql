1f0031e27960fae6b9f5ab628061811fb06bba9f
marinalis1710 <62431328+marinalis1710@users.noreply.github.com>
2020-05-20 12:15:40 +0300

    Update populate.sql

--- populate.sql
+++ populate.sql
@@ -1,16 +1,16 @@
 insert into City(city_name)
-values ("Kyiv");
+values ('Kyiv');
 insert into City(city_name)
-values ("Lviv");
+values ('Lviv');
 insert into City(city_name)
-values ("Odessa");
+values ('Odessa');
 
 insert into Barman(barman_name)
-values ("Barman Oleg");
+values ('Barman Oleg');
 insert into Barman(barman_name)
-values ("Barman Vasya");
+values ('Barman Vasya');
 insert into Barman(barman_name)
-values ("Barman Kolya");
+values ('Barman Kolya');
 
 insert into Price(price)
 values (5);
@@ -20,57 +20,59 @@ insert into Price(price)
 values (40);
 
 insert into Cocktail(cocktail_name)
-values ("Margarita");
+values ('Margarita');
 insert into Cocktail(cocktail_name)
-values ("Ocean beach");
+values ('Ocean beach');
 insert into Cocktail(cocktail_name)
-values ("Snow in winter");
+values ('Snow in winter');
+
+
 
 insert into Client(client_name)
-values ("Bob");
+values ('Bob');
 insert into Client(client_name)
-values ("Kevin");
+values ('Kevin');
 insert into Client(client_name)
-values ("Stuart");
+values ('Stuart');
 
 insert into Bar(bar_name)
-values ("Porter Pub");
+values ('Porter Pub');
 insert into Bar(bar_name)
-values ("Happy Cake");
+values ('Happy Cake');
 insert into Bar(bar_name)
-values ("Milk Bar");
+values ('Milk Bar');
 
 insert into barmanbar(barman_name, bar_name)
-values ("Barman Oleg","Milk Bar");
+values ('Barman Oleg','Milk Bar');
 insert into barmanbar(barman_name, bar_name)
-values ("Barman Kolya","Happy Cake");
+values ('Barman Kolya','Happy Cake');
 insert into barmanbar(barman_name, bar_name)
-values ("Barman Vasya","Porter Pub");
+values ('Barman Vasya','Porter Pub');
 
 insert into barmancocktail(barman_name,cocktail_name)
-values ("Barman Vasya", "Margarita");
+values ('Barman Vasya', 'Margarita');
 insert into barmancocktail(barman_name,cocktail_name)
-values ("Barman Oleg", "Ocean Beach");
+values ('Barman Oleg', 'Ocean Beach');
 insert into barmancocktail(barman_name,cocktail_name)
-values ("Barman Kolya", "Snow in winter");
+values ('Barman Kolya', 'Snow in winter');
 
 insert into Location_Of_bar(city_name, bar_name)
-values ("Kyiv", "Happy Cake");
+values ('Kyiv', 'Happy Cake');
 insert into Location_Of_bar(city_name, bar_name)
-values ("Odessa", "Milk Bar");
+values ('Odessa', 'Milk Bar');
 insert into Location_Of_bar(city_name, bar_name)
-values ("Lviv", "Porter Pub");
+values ('Lviv', 'Porter Pub');
 
 insert into clientcocktail(client_name, cocktail_name)
-values ("Bob", "Margarita");
+values ('Bob', 'Margarita');
 insert into clientcocktail(client_name, cocktail_name)
-values ("Kevin", "Ocean Beach");
+values ('Kevin', 'Ocean Beach');
 insert into clientcocktail(client_name, cocktail_name)
-values ("Stuart", "Snow in winter");
+values ('Stuart', 'Snow in winter');
 
 insert into cocktailprice(price, cocktail_name)
-values (40, "Snow in winter");
+values (40, 'Snow in winter');
 insert into cocktailprice(price, cocktail_name)
-values (10, "Ocean Beach");
+values (10, 'Ocean Beach');
 insert into cocktailprice(price, cocktail_name)
-values (5, "Margarita');
+values (5, 'Margarita');
