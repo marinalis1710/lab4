428827d3c3e1040757bb52e5e26697d60dff7938
marinalis1710 <62431328+marinalis1710@users.noreply.github.com>
2020-05-20 22:05:57 +0300

    Update procedure.sql

--- procedure.sql
+++ procedure.sql
@@ -7,35 +7,37 @@ DBMS_OUTPUT.enable;
 end;
 
 create or replace procedure DeleteFactBuying(
-client_name in clientcocktail.client_name%TYPE, 
-cocktail_name in clientcocktail.cocktail_name%TYPE, 
-rez_ out varchar2)
+clien_name in varchar2, 
+cocktai_name in varchar2, 
+rez_ out varchar2) 
 is
+my_exc exception;
 begin
-    if (client_name=clientcocktail.client_name 
-        and cocktail_name=clientcocktail.cocktail_name) then
-
-        begin
-            delete from clientcocktail
-            where client_name=clientcocktail.client_name and cocktail_name=clientcocktail.cocktail_name;
-            --model NOT IN (SELECT model FROM PC
-        end;
-    else  
-        RAISE exp; 
+    select count(*) into rez_ from clientcocktail 
+    where clientcocktail.client_name = clien_name 
+    and clientcocktail.cocktail_name = cocktai_name;
+    
+    if (rez_ > 0) then
+    
+        delete from clientcocktail
+        where clientcocktail.client_name=clien_name 
+        and clientcocktail.cocktail_name=cocktai_name;
+    else
+        --dbms_output.put_line('ERROR');
+        raise my_exc;
     end if;
-select * into rez_ from clientcocktail;
-
 exception
-   WHEN no_data_found THEN
+   WHEN my_exc THEN
       dbms_output.put_line('ERROR there is no cocktail_name or client_name'); 
 end; 
-
+--спочатку прокомпілювати процедуру, а потім запускати PL/sql код
 declare 
-rez_ varchar2;
+    rez_ varchar(500);
 
 begin
-DeleteFactBuying(client_name=>'Bob', 
-cocktail_name=>'Margarita',
-rez_=>rez_);
-DBMS_OUTPUT.PUT_LINE(rez_)
+    DeleteFactBuying(clien_name=>'Bob', 
+    cocktai_name=>'Margarita',
+    rez_ => rez_);
+    DBMS_OUTPUT.PUT_LINE(rez_);
 end;
+--для перевірки, що цей рядочок видалився можна зробити далі select по таблицы clientcocktail
