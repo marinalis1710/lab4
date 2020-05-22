26a1462777d5aeeceaaa7fc90df6e0d9ae6a662c
marinalis1710 <62431328+marinalis1710@users.noreply.github.com>
2020-05-22 11:01:10 +0300

    Update function.sql

--- function.sql
+++ function.sql
@@ -1,9 +1,10 @@
 create or replace function DeleteFactBuyingF(
     clien_name in varchar2, 
     cocktai_name in varchar2) 
-return int
+return varchar2
 is
     rez_ int;
+    message varchar2(200);
     my_exc exception;
 begin
     select count(*) into rez_ from clientcocktail 
@@ -15,12 +16,14 @@ begin
         delete from clientcocktail
         where clientcocktail.client_name=clien_name 
         and clientcocktail.cocktail_name=cocktai_name;
-        return rez_;
+        message:='Fact of bying is deleted';
+        return message;
     else
-        --dbms_output.put_line('ERROR');
-        raise my_exc;
+        raise my_exc;      
     end if;
 exception
    WHEN my_exc THEN
       dbms_output.put_line('ERROR there is no cocktail_name or client_name'); 
-end; 
+      message:='We did not find client or cocktail';
+      return message;
+end;
