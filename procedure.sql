8192f8a0fdad5100a2171e22bfb83e38ac02e9d0
marinalis1710 <62431328+marinalis1710@users.noreply.github.com>
2020-05-22 11:34:47 +0300

    Update procedure.sql

--- procedure.sql
+++ procedure.sql
@@ -1,13 +1,9 @@
-SET SERVEROUTPUT ON
-begin
-DBMS_OUTPUT.enable;
-end;
-
 create or replace procedure DeleteFactBuying(
 clien_name in varchar2, 
 cocktai_name in varchar2, 
-rez_ out varchar2) 
+message out varchar2) 
 is
+rez_ int;
 my_exc exception;
 begin
     select count(*) into rez_ from clientcocktail 
@@ -19,12 +15,12 @@ begin
         delete from clientcocktail
         where clientcocktail.client_name=clien_name 
         and clientcocktail.cocktail_name=cocktai_name;
+        message:='Fact of bying is deleted';
     else
         --dbms_output.put_line('ERROR');
         raise my_exc;
     end if;
 exception
    WHEN my_exc THEN
-      dbms_output.put_line('ERROR there is no cocktail_name or client_name'); 
+      message:='ERROR there is no cocktail_name or client_name'; 
 end; 
-
