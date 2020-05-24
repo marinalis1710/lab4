d5eb297d8abf9e5f3a881148f121929d9e97d842
marinalis1710 <62431328+marinalis1710@users.noreply.github.com>
2020-05-24 16:16:31 +0300

    Update trigger.sql

--- trigger.sql
+++ trigger.sql
@@ -1,16 +1,15 @@
 create or replace trigger Price_bfr_upd
-before update of price on cocktail
-for each row
-declare 
-my_exc EXCEPTION;
-buy_cocktail varchar2(256);
+before update on cocktail
+FOR EACH ROW
+declare
+    coc varchar2(256);
 begin 
-    select cocktail_name into buy_cocktail from clientcocktail
+    select cocktail_name into coc from clientcocktail
      where clientcocktail.cocktail_name = :old.cocktail_name AND ROWNUM = 1;
-     :new.price := :old.price;
-        dbms_output.put_line('this cocktail is sold, so can not chage cost');
+    :new.price := :old.price;
+    dbms_output.put_line('this game is sold,so can not chage cost');
     EXCEPTION
-     WHEN my_exc THEN
+     WHEN OTHERS THEN
       :new.price := :new.price;
       dbms_output.put_line('you can change');
-END;
+end;
