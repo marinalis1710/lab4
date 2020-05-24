b3973fdccd41e97960820d18c4bffa83c72bb38d
marinalis1710 <62431328+marinalis1710@users.noreply.github.com>
2020-05-24 17:15:14 +0300

    Update function.sql

--- function.sql
+++ function.sql
@@ -1,11 +1,30 @@
-create or replace function DeleteFactBuyingF(
+--перероблене view
+
+create or replace view party as 
+    select barmancocktail.barman_name,
+    barmancocktail.cocktail_name,
+    barmanbar.bar_name,
+    location_bar.city_name
+    from barmancocktail
+    inner join barmanbar on barmanbar.barman_name=barmancocktail.barman_name
+    inner join location_bar on location_bar.bar_name=barmanbar.bar_name;
+    
+create type orders_t as object(
+client_name varchar2(50),
+cocktail_name varchar(50)
+);
+
+create type order_t is table of orders_t;
+
+create or replace function DeleteFactBuyingL(
     clien_name in varchar2, 
     cocktai_name in varchar2) 
-return varchar2
+return order_t
 is
     rez_ int;
     message varchar2(200);
     my_exc exception;
+    rezult_table order_t:=order_t();
 begin
     select count(*) into rez_ from clientcocktail 
     where clientcocktail.client_name = clien_name 
@@ -13,17 +32,25 @@ begin
     
     if (rez_ > 0) then
     
-        delete from clientcocktail
-        where clientcocktail.client_name=clien_name 
-        and clientcocktail.cocktail_name=cocktai_name;
-        message:='Fact of bying is deleted';
-        return message;
+        delete from clientcocktail where clientcocktail.client_name=clien_name and clientcocktail.cocktail_name=cocktai_name;
+        dbms_output.put_line('Fact of bying is deleted');
+        for c_cursor in (select client_name, cocktail_name from clientcocktail )
+            loop
+                rezult_table.extend;
+                rezult_table(rezult_table.last) := orders_t(c_cursor.client_name, c_cursor.cocktail_name );
+            end loop;
+        return rezult_table;
     else
         raise my_exc;      
     end if;
 exception
    WHEN my_exc THEN
-
-      message:='We did not find client or cocktail';
-      return message;
+        
+       dbms_output.put_line( 'We did not find client or cocktail');
+     for c_cursor in (select client_name, cocktail_name from clientcocktail )
+            loop
+                rezult_table.extend;
+                rezult_table(rezult_table.last) := orders_t(c_cursor.client_name, c_cursor.cocktail_name );
+            end loop;
+        return rezult_table;
 end;
