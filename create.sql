dffa8d9db1e76544c881fd50949722a7166a8382
marinalis1710 <62431328+marinalis1710@users.noreply.github.com>
2020-05-24 12:27:18 +0300

    Update create.sql

--- create.sql
+++ create.sql
@@ -11,20 +11,20 @@ CREATE TABLE barman (
 ALTER TABLE barman ADD CONSTRAINT barman_pk PRIMARY KEY ( barman_name );
 
 CREATE TABLE barmanbar (
-    barman_barman_name  VARCHAR2(50) NOT NULL,
-    bar_bar_name        VARCHAR2(50) NOT NULL
+    barman_name  VARCHAR2(50) NOT NULL,
+    bar_name        VARCHAR2(50) NOT NULL
 );
 
-ALTER TABLE barmanbar ADD CONSTRAINT barmanbar_pk PRIMARY KEY ( barman_barman_name,
-                                                                bar_bar_name );
+ALTER TABLE barmanbar ADD CONSTRAINT barmanbar_pk PRIMARY KEY ( barman_name,
+                                                                bar_name );
 
 CREATE TABLE barmancocktail (
-    barman_barman_name      VARCHAR2(50) NOT NULL,
-    cocktail_cocktail_name  VARCHAR2(50) NOT NULL
+    barman_name      VARCHAR2(50) NOT NULL,
+    cocktail_name  VARCHAR2(50) NOT NULL
 );
 
-ALTER TABLE barmancocktail ADD CONSTRAINT barmancocktail_pk PRIMARY KEY ( barman_barman_name,
-                                                                          cocktail_cocktail_name );
+ALTER TABLE barmancocktail ADD CONSTRAINT barmancocktail_pk PRIMARY KEY ( barman_name,
+                                                                          cocktail_name );
 
 CREATE TABLE city (
     city_name VARCHAR2(50) NOT NULL
@@ -39,12 +39,12 @@ CREATE TABLE client (
 ALTER TABLE client ADD CONSTRAINT client_pk PRIMARY KEY ( client_name );
 
 CREATE TABLE clientcocktail (
-    client_client_name      VARCHAR2(50) NOT NULL,
-    cocktail_cocktail_name  VARCHAR2(50) NOT NULL
+    client_name      VARCHAR2(50) NOT NULL,
+    cocktail_name  VARCHAR2(50) NOT NULL
 );
 
-ALTER TABLE clientcocktail ADD CONSTRAINT clientcocktail_pk PRIMARY KEY ( client_client_name,
-                                                                          cocktail_cocktail_name );
+ALTER TABLE clientcocktail ADD CONSTRAINT clientcocktail_pk PRIMARY KEY ( client_name,
+                                                                          cocktail_name );
 
 CREATE TABLE cocktail (
     cocktail_name  VARCHAR2(50) NOT NULL,
@@ -54,41 +54,42 @@ CREATE TABLE cocktail (
 ALTER TABLE cocktail ADD CONSTRAINT cocktail_pk PRIMARY KEY ( cocktail_name );
 
 CREATE TABLE location_bar (
-    bar_bar_name    VARCHAR2(50) NOT NULL,
-    city_city_name  VARCHAR2(50) NOT NULL
+    bar_name    VARCHAR2(50) NOT NULL,
+    city_name  VARCHAR2(50) NOT NULL
 );
 
-ALTER TABLE location_bar ADD CONSTRAINT location_bar_pk PRIMARY KEY ( bar_bar_name,
-                                                                      city_city_name );
+ALTER TABLE location_bar ADD CONSTRAINT location_bar_pk PRIMARY KEY ( bar_name,
+                                                                      city_name );
 
 ALTER TABLE barmanbar
-    ADD CONSTRAINT barmanbar_bar_fk FOREIGN KEY ( bar_bar_name )
+    ADD CONSTRAINT barmanbar_bar_fk FOREIGN KEY ( bar_name )
         REFERENCES bar ( bar_name );
 
 ALTER TABLE barmanbar
-    ADD CONSTRAINT barmanbar_barman_fk FOREIGN KEY ( barman_barman_name )
+    ADD CONSTRAINT barmanbar_barman_fk FOREIGN KEY ( barman_name )
         REFERENCES barman ( barman_name );
 
 ALTER TABLE barmancocktail
-    ADD CONSTRAINT barmancocktail_barman_fk FOREIGN KEY ( barman_barman_name )
+    ADD CONSTRAINT barmancocktail_barman_fk FOREIGN KEY ( barman_name )
         REFERENCES barman ( barman_name );
 
 ALTER TABLE barmancocktail
-    ADD CONSTRAINT barmancocktail_cocktail_fk FOREIGN KEY ( cocktail_cocktail_name )
+    ADD CONSTRAINT barmancocktail_cocktail_fk FOREIGN KEY ( cocktail_name )
         REFERENCES cocktail ( cocktail_name );
 
 ALTER TABLE clientcocktail
-    ADD CONSTRAINT clientcocktail_client_fk FOREIGN KEY ( client_client_name )
+    ADD CONSTRAINT clientcocktail_client_fk FOREIGN KEY ( client_name )
         REFERENCES client ( client_name );
 
 ALTER TABLE clientcocktail
-    ADD CONSTRAINT clientcocktail_cocktail_fk FOREIGN KEY ( cocktail_cocktail_name )
+    ADD CONSTRAINT clientcocktail_cocktail_fk FOREIGN KEY ( cocktail_name )
         REFERENCES cocktail ( cocktail_name );
 
 ALTER TABLE location_bar
-    ADD CONSTRAINT location_bar_bar_fk FOREIGN KEY ( bar_bar_name )
+    ADD CONSTRAINT location_bar_bar_fk FOREIGN KEY ( bar_name )
         REFERENCES bar ( bar_name );
 
 ALTER TABLE location_bar
-    ADD CONSTRAINT location_bar_city_fk FOREIGN KEY ( city_city_name )
+    ADD CONSTRAINT location_bar_city_fk FOREIGN KEY ( city_name )
         REFERENCES city ( city_name );
+
