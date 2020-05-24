5d9989865f6902a0745a82f91e2364b728b6d84f
marinalis1710 <62431328+marinalis1710@users.noreply.github.com>
2020-05-24 18:58:36 +0300

    Update package.sql

--- package.sql
+++ package.sql
@@ -1,58 +1,72 @@
-create or replace package DeleteFactOfBuying is
-    function DeleteFactBuyingF(clien_name in varchar2, cocktai_name in varchar2) return varchar2;
-    procedure DeleteFactBuying(clien_name in varchar2, cocktai_name in varchar2, message out varchar2);
-end DeleteFactOfBuying;
 
-create or replace package body DeleteFactOfBuying is
-function DeleteFactBuyingF(clien_name in varchar2, cocktai_name in varchar2) 
-return varchar2
-is
-    rez_ int;
-    message varchar2(200);
+create or replace view party as 
+    select barmancocktail.barman_name,
+    barmancocktail.cocktail_name,
+    barmanbar.bar_name,
+    location_bar.city_name
+    from barmancocktail
+    inner join barmanbar on barmanbar.barman_name=barmancocktail.barman_name
+    inner join location_bar on location_bar.bar_name=barmanbar.bar_name;
+    
+    
+create type bars_type as object(
+    barman_name varchar2(50),
+    bar_name varchar(50)
+);
+
+create type bars_table is table of bars_type;
+
+create or replace package party_pkg is
+    type bars_str_type is record (
+    barman_name varchar2(50),
+    bar_name varchar(50));
+    
+    type bars_str_table_type is table of bars_str_type;
+    
+    function BarmanInBar(bar_n varchar2, city_n varchar2)  
+        return bars_str_table_type
+        PIPELINED;
+        
+    procedure DeleteFactBuying(clien_name in varchar2, cocktai_name in varchar2, message out varchar2) ;
+end party_pkg;
+
+create or replace package body party_pkg is
+    function BarmanInBar(bar_n varchar2, city_n varchar2)  
+    return bars_str_table_type
+    PIPELINED
+    is
+    begin 
+        for cur in ( 
+            select barman_name, bar_name from party
+            where bar_name = bar_n and city_name = city_n)
+        loop
+            pipe row(cur);
+        end loop;
+    end;
+--procedure
+    procedure DeleteFactBuying(clien_name in varchar2, cocktai_name in varchar2, message out varchar2) 
+    is rez_ int;
     my_exc exception;
-begin
-    select count(*) into rez_ from clientcocktail 
-    where clientcocktail.client_name = clien_name 
-    and clientcocktail.cocktail_name = cocktai_name;
-    
-    if (rez_ > 0) then
-    
-        delete from clientcocktail
-        where clientcocktail.client_name=clien_name 
-        and clientcocktail.cocktail_name=cocktai_name;
-        message:='Fact of bying is deleted';
-        return message;
-    else
-        raise my_exc;      
-    end if;
-exception
-   WHEN my_exc THEN
+    begin
+        select count(*) into rez_ from clientcocktail 
+        where clientcocktail.client_name = clien_name 
+        and clientcocktail.cocktail_name = cocktai_name;
+        
+        if (rez_ > 0) then
+        
+            delete from clientcocktail
+            where clientcocktail.client_name=clien_name 
+            and clientcocktail.cocktail_name=cocktai_name;
+            message:='Fact of bying is deleted';
+        else
+            raise my_exc;
+        end if;
+    exception
+       WHEN my_exc THEN
+          message:='ERROR there is no cocktail_name or client_name'; 
+    end; 
 
-      message:='We did not find client or cocktail';
-      return message;
-end; 
 
-procedure DeleteFactBuying(clien_name in varchar2, cocktai_name in varchar2, message out varchar2)
-is
-rez_ int;
-my_exc exception;
-begin
-    select count(*) into rez_ from clientcocktail 
-    where clientcocktail.client_name = clien_name 
-    and clientcocktail.cocktail_name = cocktai_name;
-    
-    if (rez_ > 0) then
-    
-        delete from clientcocktail
-        where clientcocktail.client_name=clien_name 
-        and clientcocktail.cocktail_name=cocktai_name;
-        message:='Fact of bying is deleted';
-    else
-        --dbms_output.put_line('ERROR');
-        raise my_exc;
-    end if;
-exception
-   WHEN my_exc THEN
-      message:='ERROR there is no cocktail_name or client_name'; 
-end; 
-end DeleteFactOfBuying;
+end party_pkg;
+
+
